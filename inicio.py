615035b7fd8fd89cfbc8ea76997dfcf7c74177c0
Santiago de Diego <santidediego@hotmail.com>
2015-11-04 13:23:41 +0100

    Pagina de inicio

--- inicio.py
+++ inicio.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template, request, redirect
+from wtforms import Form, BooleanField, TextField, PasswordField, TextAreaField, SelectField, RadioField, DateField, validators
 app = Flask(__name__)
+
+class RegistrationForm(Form):
+    username = TextField('Nombre de Usuario', [validators.Length(min=4, max=25)])
+    password = PasswordField('Contraseña', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='La contraseña debe coincidir con la repetición')
+    ])
+    confirm = PasswordField('Repite la contraseña')
+
 
 @app.route("/",methods=['GET', 'POST'])
 def hello():
-    return render_template("index.html")
+        form = RegistrationForm(request.form)
+        if request.method == 'POST' and form.validate():
+            return('¡Gracias %s por registrarte!' % form.username.data)
+        return render_template("index.html", form=form)
 
 if __name__ == "__main__":
     app.run()
